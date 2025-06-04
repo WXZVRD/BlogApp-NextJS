38800320e5fdfd75b883929d5f27006c5c0ac131
wxzvrd <forworknickworron@gmail.com>
2025-06-04 06:32:41 +0600

    Update: implement Header and container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/ui/Header";
+import {Container} from "@/components/ui/Container";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Header/>
+        <Container>
+            {children}
+        </Container>
       </body>
     </html>
   );
